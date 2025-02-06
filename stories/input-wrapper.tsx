5b2169cfefe73ff7a7d7c197ef760689520bf7a9
Daniel Wait <71983137+danieljwait@users.noreply.github.com>
2025-02-06 02:26:09 +0000

    fix: JSON stringify input in Temp View

--- stories/input-wrapper.tsx
+++ stories/input-wrapper.tsx
@@ -15,7 +15,7 @@ export function wrapInput<P>(Input: React.FC<P>): React.FC<any> {
           } else {
             responseString = newResponse?.toString() ?? ''
           }
-          localStorage.setItem('student.input', responseString)
+          localStorage.setItem('student.input', JSON.stringify(responseString))
         }}
       />
     )
