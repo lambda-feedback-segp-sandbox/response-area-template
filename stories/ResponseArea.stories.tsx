d525fdbae81244bb08a28282f6f4de6444c40fab
Alex Shem <as7322@ic.ac.uk>
2025-02-06 12:08:06 +0000

    Modified ResponseArea.stories to read wizard config

--- stories/ResponseArea.stories.tsx
+++ stories/ResponseArea.stories.tsx
@@ -1,4 +1,5 @@
 import { ResponseAreaView } from '@lambda-feedback-segp-sandbox/response-area/components/ResponseAreaView.component'
+import { IModularResponseSchema } from '@lambda-feedback-segp-sandbox/response-area-base/schemas/question-form.schema'
 import {
   Delete as DeleteIcon,
   BarChart,
@@ -7,13 +8,64 @@ import {
 } from '@mui/icons-material'
 import Button from '@mui/material/Button'
 import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import React, { useState } from 'react'
 
 import { MyResponseAreaTub } from '../components'
 
 import { wrapInput } from './input-wrapper'
 
+const initialiseResponseArea = (args: any): React.FC<any> => {
+  return () => {
+    const [response] = useState<IModularResponseSchema | null>(() => {
+      const storedResponse = localStorage.getItem("wizard.input");
+      if (storedResponse) {
+        try {
+          const parsedResponse: IModularResponseSchema = JSON.parse(storedResponse);
+          return parsedResponse && parsedResponse.config ? parsedResponse : null;
+        } catch {
+          return null; // Return null if JSON parsing fails
+        }
+      }
+      return null;
+    });
+
+    const templateResponseAreaTub = new MyResponseAreaTub();
+    if (response && response.config) {
+      // @ts-ignore
+      templateResponseAreaTub.config = response.config;
+    } else {
+      templateResponseAreaTub.initWithDefault();
+    }
+
+    return templateResponseAreaTub.InputComponent({
+      ...args,
+      handleChange: (val: IModularResponseSchema) => {
+        if (val) {
+          localStorage.setItem("student.input", JSON.stringify(val));
+        }
+      },
+    });
+  };
+};
+
+const InputMeta: Meta = {
+  title: "Input",
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    handleChange: (val: IModularResponseSchema) => {
+      if (val && val.config && val.answer) {
+        localStorage.setItem("student.input", JSON.stringify(val));
+      }
+    },
+    handleSubmit: fn(),
+  },
+};
+
 const tub = new MyResponseAreaTub()
-tub.InputComponent = wrapInput(tub.InputComponent)
+tub.InputComponent = wrapInput(initialiseResponseArea(InputMeta.args))
 const ResponseAreaViewMeta = {
   title: 'Response Area',
   component: ResponseAreaView,
