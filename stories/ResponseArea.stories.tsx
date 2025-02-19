5c036e490c16784c364ed85984841875c6e7b3cf
Alexey Popov <alexey.popov22@imperial.ac.uk>
2025-02-19 15:41:21 +0000

    fix: Resolve hook bug

--- stories/ResponseArea.stories.tsx
+++ stories/ResponseArea.stories.tsx
@@ -21,33 +21,33 @@ import { MyResponseAreaTub } from '../components'
 import { wrapInput } from './input-wrapper'
 
 const InitialiseResponseArea: React.FC<any> = (args: any) => {
-    const [response] = useState<IModularResponseSchema | null>(() => {
-      const storedResponse = localStorage.getItem("wizard.input");
-      if (storedResponse) {
-        try {
-          const parsedResponse: IModularResponseSchema = JSON.parse(storedResponse);
-          return parsedResponse && parsedResponse.config ? parsedResponse : null;
-        } catch {
-          return null; // Return null if JSON parsing fails
-        }
+  const [response] = useState<IModularResponseSchema | null>(() => {
+    const storedResponse = localStorage.getItem("wizard.input");
+    if (storedResponse) {
+      try {
+        const parsedResponse: IModularResponseSchema = JSON.parse(storedResponse);
+        return parsedResponse && parsedResponse.config ? parsedResponse : null;
+      } catch {
+        return null; // Return null if JSON parsing fails
       }
-      return null;
-    });
-
-    const templateResponseAreaTub = new MyResponseAreaTub();
-    if (response && response.config) {
-      // @ts-ignore
-      templateResponseAreaTub.config = response.config;
-    } else {
-      templateResponseAreaTub.initWithDefault();
     }
-    return <templateResponseAreaTub.InputComponent {...args}
-              handleChange={(val: IModularResponseSchema) => {
-                  if (val) {
-                      localStorage.setItem("student.input", JSON.stringify(val));
-                  }
-              }} />;
-  };
+    return null;
+  });
+
+  const templateResponseAreaTub = new MyResponseAreaTub();
+  if (response && response.config) {
+    // @ts-ignore
+    templateResponseAreaTub.config = response.config;
+  } else {
+    templateResponseAreaTub.initWithDefault();
+  }
+  return <templateResponseAreaTub.InputComponent {...args}
+                                                 handleChange={(val: IModularResponseSchema) => {
+                                                   if (val) {
+                                                     localStorage.setItem("student.input", JSON.stringify(val));
+                                                   }
+                                                 }} />;
+};
 
 const tub = new MyResponseAreaTub()
 tub.InputComponent = wrapInput(InitialiseResponseArea)
@@ -69,6 +69,103 @@ const ResponseAreaViewMeta = {
 export default ResponseAreaViewMeta
 type Story = StoryObj<typeof ResponseAreaViewMeta>
 
+const TempViewComponent = (args: any) => {
+  const [open, setOpen] = useState(false);
+  const [dialogContent, setDialogContent] = useState({
+    title: '',
+    description: '',
+  });
+
+  const handleButtonClick = (buttonType: string) => {
+    let title = 'Not Available';
+    let description = `${buttonType} is not available in SandBox`;
+
+    setDialogContent({ title, description });
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <ResponseAreaView
+        {...args}
+        handleCheck={() => handleButtonClick('Check')}
+        handleDraftSave={() => handleButtonClick('Save')}
+        ActionButtons={
+          <>
+            <Button
+              variant="outlined"
+              endIcon={<Tune />}
+              onClick={() => handleButtonClick('Configure')}
+            >
+              Configure
+            </Button>
+            <Button
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+              variant="outlined"
+              endIcon={<BarChart />}
+              onClick={() => handleButtonClick('Explore')}
+            >
+              Explore
+            </Button>
+            <Button
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+              variant="outlined"
+              endIcon={<ContentCopy />}
+              onClick={() => handleButtonClick('Duplicate')}
+            >
+              Duplicate
+            </Button>
+            <Button
+              color="error"
+              variant="outlined"
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+              }}
+              endIcon={<DeleteIcon />}
+              onClick={() => handleButtonClick('Delete')}
+            >
+              Delete
+            </Button>
+          </>
+        }
+      />
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{dialogContent.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {dialogContent.description}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
 export const tempView: Story = {
   args: {
     tub: tub,
@@ -86,100 +183,5 @@ export const tempView: Story = {
     universalResponseAreaId: '00000000-0000-0000-0000-000000000000',
     wrapLabel: 'Area Label',
   },
-  render: (args) => {
-    const [open, setOpen] = useState(false);
-    const [dialogContent, setDialogContent] = useState({
-      title: '',
-      description: '',
-    });
-
-    const handleButtonClick = (buttonType: string) => {
-      let title = 'Not Available';
-      let description = `${buttonType} is not available in SandBox`;
-
-      setDialogContent({ title, description });
-      setOpen(true);
-    };
-
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    return (
-      <>
-        <ResponseAreaView
-          {...args}
-          handleCheck={() => handleButtonClick('Check')}
-          handleDraftSave={() => handleButtonClick('Save')}
-          ActionButtons={
-            <>
-              <Button
-                variant="outlined"
-                endIcon={<Tune />}
-                onClick={() => handleButtonClick('Configure')}
-              >
-                Configure
-              </Button>
-              <Button
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}
-                variant="outlined"
-                endIcon={<BarChart />}
-                onClick={() => handleButtonClick('Explore')}
-              >
-                Explore
-              </Button>
-              <Button
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}
-                variant="outlined"
-                endIcon={<ContentCopy />}
-                onClick={() => handleButtonClick('Duplicate')}
-              >
-                Duplicate
-              </Button>
-              <Button
-                color="error"
-                variant="outlined"
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  width: '100%',
-                }}
-                endIcon={<DeleteIcon />}
-                onClick={() => handleButtonClick('Delete')}
-              >
-                Delete
-              </Button>
-            </>
-          }
-        />
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{dialogContent.title}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              {dialogContent.description}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary" autoFocus>
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </>
-    );
-  },
+  render: (args) => <TempViewComponent {...args} />,
 }
