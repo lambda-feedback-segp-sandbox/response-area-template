77a3158030292ce29e5d3af80da12f723c9a476e
Daniel Trowbridge <danieljtrowbridge@pm.me>
2025-02-24 17:39:25 +0000

    feat: minify production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,26 @@
 const path = require('path')
 
 module.exports = {
-    entry: './components/WebComponents.tsx',
-    output: {
-        filename: 'web-components.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        alias: {'next/font/google': path.resolve(__dirname, 'mocks/fonts.ts')},
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                options: {"allowTsInNodeModules": true}
-            },
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-            },
-        ],
-    },
-    optimization: {
-        minimize: false
-    }
-};
+  entry: './components/WebComponents.tsx',
+  output: {
+    filename: 'web-components.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: { 'next/font/google': path.resolve(__dirname, 'mocks/fonts.ts') },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: { allowTsInNodeModules: true },
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+}
