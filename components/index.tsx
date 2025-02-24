ae314c15a9c309b987753b5225637d62037a1380
Rory Probert <rp922@ic.ac.uk>
2025-02-24 18:15:24 +0000

    fix: fix return type for tub InputComponent and WizardComponent

--- components/index.tsx
+++ components/index.tsx
@@ -3,6 +3,7 @@ import {
   BaseResponseAreaWizardProps,
 } from '@lambda-feedback-segp-sandbox/response-area-base/types/base-props.type'
 import { ResponseAreaTub } from '@lambda-feedback-segp-sandbox/response-area-base/types/response-area-tub'
+import { JSX } from 'react'
 import { z } from 'zod'
 
 import { inputConfigSchema, inputResponseAnswerSchema } from './Input.schema'
@@ -43,44 +44,54 @@ export class MyResponseAreaTub extends ResponseAreaTub {
     this.InputComponent = this.InputComponent.bind(this);
     this.WizardComponent = this.WizardComponent.bind(this);
   }
-
-  /** Creates a main response area component, instantiating a student and
-   *  teacher preview views. {@link BaseResponseAreaProps}
-   *  @param props - Base parameters passed to all response areas
-   *  @returns ReactNode rendering the view
-   *  */
-  InputComponent(
-    props: BaseResponseAreaProps,
-  ): HTMLElement {
+  InputComponent(props: BaseResponseAreaProps): JSX.Element {
     this.config = this.config ?? { fontFamily: 'Arial' };
     (window as any)[`RA_${RESPONSE_TYPE}_handleChange`] = props.handleChange;
     (window as any)[`RA_${RESPONSE_TYPE}_handleSubmit`] = props.handleSubmit;
     (window as any)[`RA_${RESPONSE_TYPE}_handleDraftSave`] = props.handleDraftSave;
     (window as any)[`RA_${RESPONSE_TYPE}_previewSubmit`] = props.previewSubmit;
-    return <input-component config={JSON.stringify(this.config)} answer={this.answer}
-     display-mode={props.displayMode} response-area-id={props.responseAreaId}
-     universal-response-area-id={props.universalResponseAreaId} has-preview={props.hasPreview}
-     is-teacher-mode={props.isTeacherMode} pre-response-text={props.preResponseText} 
-     post-response-text={props.postResponseText} check-is-loading={props.checkIsLoading} 
-     feedback={JSON.stringify(props.feedback)} typesafe-error-message={props.typesafeErrorMessage}
-     handle-change={`RA_${RESPONSE_TYPE}_handleChange`} handle-submit={`RA_${RESPONSE_TYPE}_handleSubmit`}
-     handle-draft-save={`RA_${RESPONSE_TYPE}_handleDraftSave`} preview-submit={`RA_${RESPONSE_TYPE}_previewSubmit`}
-     style={{width: "100%", height: "100%"}}/>
-  }
 
+    return (
+      <input-component
+        config={JSON.stringify(this.config)}
+        answer={this.answer}
+        display-mode={props.displayMode}
+        response-area-id={props.responseAreaId}
+        universal-response-area-id={props.universalResponseAreaId}
+        has-preview={props.hasPreview}
+        is-teacher-mode={props.isTeacherMode}
+        pre-response-text={props.preResponseText}
+        post-response-text={props.postResponseText}
+        check-is-loading={props.checkIsLoading}
+        feedback={JSON.stringify(props.feedback)}
+        typesafe-error-message={props.typesafeErrorMessage}
+        handle-change={`RA_${RESPONSE_TYPE}_handleChange`}
+        handle-submit={`RA_${RESPONSE_TYPE}_handleSubmit`}
+        handle-draft-save={`RA_${RESPONSE_TYPE}_handleDraftSave`}
+        preview-submit={`RA_${RESPONSE_TYPE}_previewSubmit`}
+        style={{ width: "100%", height: "100%" }}
+      />
+    );
+  }
+  
   /** Creates a teacher view, allowing configuration of the response area.
-   *  {@link BaseResponseAreaProps}
-   *  @param props - Base parameters passed to all response areas
-   *  @returns ReactNode rendering the view
-   *  */
-  WizardComponent(
-    props: BaseResponseAreaWizardProps,
-  ): HTMLElement {
-    if (!this.config) throw new Error('Config missing')
-    window[`RA_${RESPONSE_TYPE}_handleChange`] = props.handleChange
-    window[`RA_${RESPONSE_TYPE}_setAllowSave`] = props.setAllowSave
-    return <wizard-component config={JSON.stringify(this.config)} answer={this.answer} 
-            handle-change={`RA_${RESPONSE_TYPE}_handleChange`} set-allow-save={`RA_${RESPONSE_TYPE}_setAllowSave`}
-            style={{width: "100%", height: "100%"}}/>
+ *  {@link BaseResponseAreaProps}
+ *  @param props - Base parameters passed to all response areas
+ *  @returns ReactNode rendering the view
+ *  */
+  WizardComponent(props: BaseResponseAreaWizardProps): JSX.Element {
+    if (!this.config) throw new Error('Config missing');
+    (window as any)[`RA_${RESPONSE_TYPE}_handleChange`] = props.handleChange;
+    (window as any)[`RA_${RESPONSE_TYPE}_setAllowSave`] = props.setAllowSave;
+
+    return (
+      <wizard-component
+        config={JSON.stringify(this.config)}
+        answer={this.answer}
+        handle-change={`RA_${RESPONSE_TYPE}_handleChange`}
+        set-allow-save={`RA_${RESPONSE_TYPE}_setAllowSave`}
+        style={{ width: "100%", height: "100%" }}
+      />
+    );
   }
 }
