807ec6314f421007f01837111b200153df115d15
Daniel Trowbridge <danieljtrowbridge@pm.me>
2025-03-01 17:24:45 +0000

    chore: remove commented-out code and console.logs

--- components/Wizard.component.tsx
+++ components/Wizard.component.tsx
@@ -1,14 +1,12 @@
 import { IModularResponseSchema } from '@lambda-feedback-segp-sandbox/response-area/schemas/question-form.schema'
 import { BaseResponseAreaWizardProps } from '@lambda-feedback-segp-sandbox/response-area-base/types/base-props.type'
-//import { makeStyles } from '@lambda-feedback-segp-sandbox/styles'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
 import React from 'react'
 
-// @ts-ignore
-import { Input, InputProps } from './Input.component'
+import { Input } from './Input.component'
 
 /** Custom input parameters for the Wizard component, extending or overriding
  *  parameters provided in {@link BaseResponseAreaWizardProps} */
@@ -27,76 +25,47 @@ export const Wizard: React.FC<WizardProps> = ({
   handleChange,
   answer,
   config,
-}) => {
-  console.log('Answer in Wizard: ', answer)
-  console.log('Config in Wizard: ', config)
-
-  // The following code is for demonstration purposes only, it can be
-  // completely refactored
-  //const { classes } = useStyles()
-
-  return (
-    <div>
-      <div>
-        <FormControl variant="outlined">
-          <InputLabel id="font-select-label">Font</InputLabel>
-          <Select
-            labelId="font-select-label"
-            onChange={event => {
-              handleChange({
-                config: { fontFamily: event.target.value },
-                answer: answer ?? '',
-                responseType: 'REPLACE_ME',
-              })
-            }}
-            defaultValue={config.fontFamily}
-            label="Font">
-            <MenuItem value="Arial" sx={{ fontFamily: 'Arial' }}>
-              Arial
-            </MenuItem>
-            <MenuItem value="Courier New" sx={{ fontFamily: 'Courier New' }}>
-              Courier New
-            </MenuItem>
-            <MenuItem
-              value="Times New Roman"
-              sx={{ fontFamily: 'Times New Roman' }}>
-              Times New Roman
-            </MenuItem>
-            <MenuItem value="Cursive" sx={{ fontFamily: 'Cursive' }}>
-              Cursive
-            </MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-      <Input
-        handleChange={response =>
+}) => (
+  <>
+    <FormControl variant="outlined">
+      <InputLabel id="font-select-label">Font</InputLabel>
+      <Select
+        labelId="font-select-label"
+        onChange={event => {
           handleChange({
-            config,
-            answer: response,
+            config: { fontFamily: event.target.value },
+            answer: answer ?? '',
             responseType: 'REPLACE_ME',
           })
-        }
-        answer={answer}
-        config={config as { fontFamily: string }}
-      />
-    </div>
-  )
-}
-/*
-const useStyles = makeStyles()(theme => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: theme.spacing(2),
-    padding: theme.spacing(2),
-  },
-  toolbar: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    gap: theme.spacing(2),
-  },
-  formControl: {
-    minWidth: 120,
-  },
-}));*/
+        }}
+        defaultValue={config.fontFamily}
+        label="Font">
+        <MenuItem value="Arial" sx={{ fontFamily: 'Arial' }}>
+          Arial
+        </MenuItem>
+        <MenuItem value="Courier New" sx={{ fontFamily: 'Courier New' }}>
+          Courier New
+        </MenuItem>
+        <MenuItem
+          value="Times New Roman"
+          sx={{ fontFamily: 'Times New Roman' }}>
+          Times New Roman
+        </MenuItem>
+        <MenuItem value="Cursive" sx={{ fontFamily: 'Cursive' }}>
+          Cursive
+        </MenuItem>
+      </Select>
+    </FormControl>
+    <Input
+      handleChange={response =>
+        handleChange({
+          config,
+          answer: response,
+          responseType: 'REPLACE_ME',
+        })
+      }
+      answer={answer}
+      config={config as { fontFamily: string }}
+    />
+  </>
+)
