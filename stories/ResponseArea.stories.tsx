9e447fa0d4626e14f8f6b9ce60ad784eb2aa31be
Daniel Trowbridge <danieljtrowbridge@pm.me>
2025-03-04 18:32:47 +0000

    feat: add Custom Feedback story from response-area-template-lib

--- stories/ResponseArea.stories.tsx
+++ stories/ResponseArea.stories.tsx
@@ -3,6 +3,7 @@
 import {
   AllActionButtonsDisabledStory,
   AllActionButtonsEnabledStory,
+  CustomFeedbackStory,
   ExploreActionButtonDisabledStory,
   StudentViewStory,
   createMeta,
@@ -27,6 +28,9 @@ export const ExploreActionButtonDisabled = ExploreActionButtonDisabledStory
 
 // Managed by response-area-template-lib.
 export const AllActionButtonsDisabled = AllActionButtonsDisabledStory
+
+// Managed by response-area-template-lib.
+export const CustomFeedback = CustomFeedbackStory
 
 // You can add your own stories here.
 // See https://storybook.js.org/docs/writing-stories#how-to-write-stories.
